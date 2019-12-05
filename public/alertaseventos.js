61dbadccb177b84c63fdd7ac3fa9e717d262684c
felipesilvaPW <fss2@icomp.ufam.edu.br>
2019-12-05 04:13:40 -0400

    att

--- public/alertaseventos.js
+++ public/alertaseventos.js
@@ -16,6 +16,8 @@ socketzinho.on('serial:data', function(dataSerial){
     if(pir == 1){ 
         alert("Sensor detectou presença!");
         $('.alert').alert()
+        $('#pirCard').text('MOVIMENTO DETECTADO!!!');
+        $("#pirCard").addClass("alert-danger").removeClas("alert-success");
     }
     
     arr[0] = arr[0].substring(1);
@@ -31,18 +33,40 @@ socketzinho.on('serial:data', function(dataSerial){
     }
 
     contador = contador + 2;
+
     const call = io();
-    if(tempDHT22 > 54 || tempDHT11 > 54){
+    if(tempDHT22 > 54){
         alert("Temperatura muito alta, risco de incendio!");
         $('.alert').alert()
+        $('#temp22Card').text('Temperatura ALTA!! ALERTA!');
+        $("#temp22Card").addClass("alert-danger").removeClas("alert-success");
+    }
+
+    if(tempDHT11 > 54){
+        alert("Temperatura muito alta, risco de incendio!");
+        $('.alert').alert()
+        $('#temp11Card').text('Temperatura ALTA!! ALERTA!');
+        $("#temp11Card").addClass("alert-danger").removeClas("alert-success");
     }
 
     if(contador >= 30){
-        if((tempDHT22 - guardaTemp22) > 5  || (tempDHT11 - guardaTemp11) > 5){
+        if((tempDHT22 - guardaTemp22) > 5){
             alert("Temperatura aumetou drasticamente, risco de incendio!");
             $('.alert').alert()
+            $('#temp22Card').text('Temperatura ALTA!! ALERTA!');
+            $("#temp22Card").addClass("alert-danger").removeClas("alert-success");
         }
         contador = 0;
+    }
+    
+    if(contador >= 30){
+        if((tempDHT11 - guardaTemp11) > 5){
+            alert("Temperatura aumetou drasticamente, risco de incendio!");
+            $('.alert').alert()
+            $('#temp11Card').text('Temperatura ALTA!! ALERTA!');
+            $("#temp11Card").addClass("alert-danger").removeClas("alert-success");
+            contador = 0;
+        }
     }
 });
 
